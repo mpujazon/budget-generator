5d2b6f36966d4b862bcda1a4d8751887c48af6a1
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-02-11 10:08:41 +0100

    Feature: Homepage now is supporting light and dark modes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Usa 'selector' (para Tailwind v3.4+) o 'class' (para versiones anteriores)
+  darkMode: 'selector',
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
