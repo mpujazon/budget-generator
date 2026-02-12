c5c693a2a557aeeeccbddb9078348ea44fb2285a
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-02-12 09:11:51 +0100

    Feat: ServiceCard Component Finnished

--- src/app/components/service-card/service-card.ts
+++ src/app/components/service-card/service-card.ts
@@ -1,16 +1,9 @@
 import {Component, input} from '@angular/core';
-import {Card} from 'primeng/card';
-import {Button} from 'primeng/button';
 import {ServiceItem} from '../../interfaces/ServiceItem';
-import {InputNumber} from 'primeng/inputnumber';
 
 @Component({
   selector: 'app-service-card',
-  imports: [
-    Card,
-    Button,
-    InputNumber
-  ],
+  imports: [],
   templateUrl: './service-card.html'
 })
 export class ServiceCard {
