4cd290f7f276457c45350ec0036da631fba5fd76
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-02-17 18:58:22 +0100

    Refactor: Improved number inputs in ServiceCard

--- src/app/components/service-card/service-card.ts
+++ src/app/components/service-card/service-card.ts
@@ -1,16 +1,18 @@
 import {Component, input, output} from '@angular/core';
 import {ServiceItem, ServiceUpdates} from '../../interfaces/ServiceItem';
+import {InputNumberModule} from 'primeng/inputnumber';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-service-card',
-  imports: [],
+  imports: [InputNumberModule, FormsModule],
   templateUrl: './service-card.html'
 })
 export class ServiceCard {
   service = input.required<ServiceItem>();
   onUpdateService = output<ServiceUpdates>();
 
-  addService(event: Event) {
+  addService() {
     this.service().selected = !this.service().selected;
     const updateServiceItem = {
       id: this.service().id,
@@ -19,18 +21,19 @@ export class ServiceCard {
     this.onUpdateService.emit(updateServiceItem);
   }
 
-  updateService(event: Event){
-    const inputNumber = event.target as HTMLInputElement;
-    const propertyName = inputNumber.name;
-
-    if (propertyName !== "pages" && propertyName !== "languages") {
-      console.warn("Invalid property name");
-      return;
-    }
+  updatePages(value: number | null) {
     const updateServiceItem = {
-        id: this.service().id,
-        [propertyName]: inputNumber.valueAsNumber
-      }
+      id: this.service().id,
+      pages: value ?? 1
+    }
+    this.onUpdateService.emit(updateServiceItem);
+  }
+
+  updateLanguages(value: number | null) {
+    const updateServiceItem = {
+      id: this.service().id,
+      languages: value ?? 1
+    }
     this.onUpdateService.emit(updateServiceItem);
   }
 }
