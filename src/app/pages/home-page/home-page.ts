03022c77d099d572d79a7b75709e078455dd491b
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-02-19 12:52:22 +0100

    Refactor:'Start Your Project' button on homepage reuses Button components

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import {Button} from '../../components/atoms/button/button';
+import {routes} from '../../app.routes';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [
+    Button,
+    RouterLink
+  ],
   templateUrl: './home-page.html'
 })
 export class HomePage {
 
+  protected readonly routes = routes;
 }
